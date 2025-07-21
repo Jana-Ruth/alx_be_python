325f235118e861327f88d8c45de45d2d386db47a
Jana-Ruth <168997071+Jana-Ruth@users.noreply.github.com>
2025-07-21 08:59:16 +0100

    f"

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,4 +2,4 @@ x = int(input("Enter a number to see its multiplication table:"))
 
 
 for number in range(1, 11):
-    print(x, "*", number, "=", 5*number)
+    print(f"x, "*", number, "=", 5*number)
