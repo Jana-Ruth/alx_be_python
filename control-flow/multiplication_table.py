aed060471e8d288236d496d38f75e50b97d48e07
Jana-Ruth <168997071+Jana-Ruth@users.noreply.github.com>
2025-07-21 09:02:35 +0100

    add *. properly

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,4 +2,4 @@ x = int(input("Enter a number to see its multiplication table:"))
 
 
 for number in range(1, 11):
-    print(f"x, "*", number, "=", 5*number)
+    print(f"number, "*" x, , "=", x * number)
