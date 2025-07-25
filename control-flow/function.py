b940d4bfaeec4a35b42617351d2bf8474a12042e
Jana-Ruth <ruthewenode73@gmail.com>
2025-07-25 17:05:20 +0100

    week 4 project

---
+++ control-flow/function.py
@@ -0,0 +1,18 @@
+def myName(name):
+    print(f"Hello {name}, good morning")
+    
+    me=f"Hello {name}, good morning"
+result = myName("Ruth")   
+
+print(result) 
+
+
+
+def sameVariable():
+    count = 1
+    print(count)
+    
+sameVariable()
+
+count = 4
+print(count)    
--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -3,3 +3,10 @@ x = 5
 
 for number in range(1, 11):
     print(x, "*", number, "=", 5*number)
+    
+def calculate_area(length, width):
+    """Calculates the area of a rectangle."""
+    area = length * width
+    print(area)
+calculate_area(5,10)
+
