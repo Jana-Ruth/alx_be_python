541ce232bf13a12d2a28e62e412e9b47bbc2b4b9
Jana-Ruth <ruthewenode73@gmail.com>
2025-08-03 00:07:46 +0100

    library management

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,8 +1,8 @@
 class Book:
     def __init__(self, title, author):
-        self.title = title
-        self.author = author
-        self._is_checked_out = False
+        self.title = title         # Public attribute
+        self.author = author       # Public attribute
+        self._is_checked_out = False  # Private attribute
 
     def check_out(self):
         if not self._is_checked_out:
@@ -21,3 +21,39 @@ class Book:
 
     def __str__(self):
         return f"{self.title} by {self.author}"
+
+
+class Library:
+    def __init__(self):
+        self._books = []  # Private list to store Book objects
+
+    def add_book(self, book):
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        for book in self._books:
+            if book.title == title:
+                if book.check_out():
+                    return
+                else:
+                    print(f"'{title}' is already checked out.")
+                    return
+        print(f"Book titled '{title}' not found in the library.")
+
+    def return_book(self, title):
+        for book in self._books:
+            if book.title == title:
+                if book.return_book():
+                    return
+                else:
+                    print(f"'{title}' was not checked out.")
+                    return
+        print(f"Book titled '{title}' not found in the library.")
+
+    def list_available_books(self):
+        available_books = [book for book in self._books if book.is_available()]
+        if available_books:
+            for book in available_books:
+                print(book)
+        else:
+            print("No available books at the moment.")
